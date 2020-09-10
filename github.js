a35049f09f16f60699ac446bd58b0fe3498b885b
ryan woods <federer5050@gmail.com>
2020-09-10 14:26:20 +0100

    adding UI

--- github.js
+++ github.js
@@ -5,6 +5,16 @@ class Github {
     }
 
     async getUser(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=`);
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+    
+        return {
+          profile,
+          repos
+        }
+      }
     }
-}
